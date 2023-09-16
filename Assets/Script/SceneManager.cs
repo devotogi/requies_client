52fc623458626348e777a53ee6655b5c68d07d1a
devotogi <141953029+devotogi@users.noreply.github.com>
2023-09-16 22:47:11 +0900

    grid

--- Assets/Script/SceneManager.cs
+++ Assets/Script/SceneManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class SceneManager : MonoBehaviour
 { 
@@ -11,5 +12,18 @@ public class SceneManager : MonoBehaviour
         GameObject network = new GameObject();
         network.name = "Network";
         network.AddComponent<Network>();
+
+        for (int z = 0; z <= 256; z++)
+        {
+            for (int x = 0; x <= 256; x++)
+            {
+                if (z % 32 == 0 || x % 32 == 0) 
+                {
+                    GameObject wallPrefab = Resources.Load<GameObject>($"Prefabs/WALL");
+                    GameObject wallgo = Object.Instantiate(wallPrefab);
+                    wallgo.transform.position = new Vector3(x, 0, z);
+                }
+            }
+        }
     }
 }
